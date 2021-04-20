bc29268869d13f5a7a0fd07929dbe9d528aac700
Pedro368 <56154168+Pedro368@users.noreply.github.com>
2021-04-20 13:35:27 +0200

    Cambios de código

--- ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola a todos!");
         }
     }
 }
